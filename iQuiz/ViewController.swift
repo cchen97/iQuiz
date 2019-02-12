2f659f492f448c09a8de3af01fe0eff982749eab
cchen97 <manjic@uw.edu>
2019-02-12 11:29:17 -0800

    ?

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Charlene on 2/12/19.
+//  Copyright © 2019 Charlene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
