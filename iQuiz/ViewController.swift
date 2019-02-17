ca39729294dba8aceb1a5f0c48294c0e0b16c1bc
cchen97 <manjic@uw.edu>
2019-02-17 11:05:18 -0800

    part1

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,10 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var quizzes = ["Mathematics", "Marvel Super Heroes", "Science"]
+    var quizDetails = ["Challenge yourself with math questions!", "Come have fun with superhero questions!", "Love science? Try our science questions!"]
+    var images : [UIImage] = [UIImage(named: "1.png")!,UIImage(named: "2.png")!,UIImage(named: "3.png")! ]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "reusedCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: identifier)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: identifier)
+        }
+        let rowNum = indexPath.row
+        cell?.textLabel?.text = quizzes[rowNum]
+        cell?.detailTextLabel?.text = quizDetails[rowNum]
+        cell?.imageView?.image = images[rowNum]
+        return cell!
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let rect = CGRect(x: 0, y: 150, width: 400, height: 800)
+        let tableView = UITableView(frame: rect)
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview((tableView))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,7 +51,7 @@ class ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
             NSLog("The \"OK\" alert occured.")
         }))
-        
+
         self.present(alert, animated: true, completion: {
             print("This is hte completion handler for the present() code")
         })
