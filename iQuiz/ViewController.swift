c431a7d7f93bf060e36ac75f500cf521219e03e3
cchen97 <manjic@uw.edu>
2019-02-17 09:13:52 -0800

    init

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func alert(_ sender: Any) {
+        let alert = UIAlertController(title: "My Alert", message: "Settings go here", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        
+        self.present(alert, animated: true, completion: {
+            print("This is hte completion handler for the present() code")
+        })
+    }
+    
+ 
+    
 }
 
