560504104f869acb8fa25082241d34066df1e2ed
cchen97 <manjic@uw.edu>
2019-02-26 22:07:26 -0800

    done

--- iQuiz/Finished.swift
+++ iQuiz/Finished.swift
@@ -15,14 +15,25 @@ class Finished: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let questionText = UILabel(frame: CGRect(x: 50, y: 50, width: 200, height: 200))
+        let questionText = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
         questionText.text = "Perfect!"
-        let percentText = UILabel(frame: CGRect(x: 50, y: 300, width: 400, height: 400))
+        questionText.translatesAutoresizingMaskIntoConstraints = true
+        questionText.center = CGPoint(x: view.bounds.midX, y: 50)
+        questionText.autoresizingMask = [UIView.AutoresizingMask.flexibleLeftMargin, UIView.AutoresizingMask.flexibleRightMargin, UIView.AutoresizingMask.flexibleTopMargin, UIView.AutoresizingMask.flexibleBottomMargin]
+        
+        let percentText = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
         percentText.text = "You got " + String(correct) + " out of " + String(totalQuestions) + " right!"
-        let nextButton = UIButton(frame: CGRect(x: 50, y: 500, width: 200, height: 200))
+        percentText.translatesAutoresizingMaskIntoConstraints = true
+        percentText.center = CGPoint(x: view.bounds.midX, y: 150)
+        percentText.autoresizingMask = [UIView.AutoresizingMask.flexibleLeftMargin, UIView.AutoresizingMask.flexibleRightMargin, UIView.AutoresizingMask.flexibleTopMargin, UIView.AutoresizingMask.flexibleBottomMargin]
+        
+        let nextButton = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
         nextButton.setTitle("Back to home", for: .normal)
         nextButton.setTitleColor(UIColor.blue, for: .normal)
         nextButton.addTarget(self, action: #selector(pressedAction(_:)), for: .touchUpInside)
+        nextButton.translatesAutoresizingMaskIntoConstraints = true
+        nextButton.center = CGPoint(x: view.bounds.midX, y: 200)
+        nextButton.autoresizingMask = [UIView.AutoresizingMask.flexibleLeftMargin, UIView.AutoresizingMask.flexibleRightMargin, UIView.AutoresizingMask.flexibleTopMargin, UIView.AutoresizingMask.flexibleBottomMargin]
         self.view.addSubview(questionText)
         self.view.addSubview(percentText)
         self.view.addSubview(nextButton)
